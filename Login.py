607667492f69d7a07a3d45fad6bacf6833329157
xyber0801 <sikea0801@gmail.com>
2023-12-16 10:33:20 +0700

    Send email, check same picture, avoid registering with the same username

--- Login.py
+++ Login.py
@@ -2,6 +2,11 @@ import PySimpleGUI as sg
 import gspread 
 import face_recognition
 import numpy as np
+import random
+import constants as c
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
@@ -12,13 +17,29 @@ sh = gc.open_by_url("https://docs.google.com/spreadsheets/d/1d7Yf2fj7tr4udMj7Jg2
 
 worksheet = sh.sheet1
 
+#Khai báo hằng số
+EMAIL_COLUMN = 1
+USERNAME_COLUMN = 2
+PASSWORD_COLUMN = 3
+FACE_ID_COLUMN = 4
+MONEY_COLUMN = 5
+LANGUAGE_COLUMN = 6
+BUFF_COLUMN = 7
+WINRATE_COLUMN = 8
+
+
+#Hàm lưu dữ liệu
+def Save (row, column, Svalue):
+    worksheet.update_cell(row, column, Svalue)
+
 #Hàm lấy dữ liệu FaceID từ database
 def LoadFaceID(Fpassword):
     Fpassword = str(Fpassword).replace("[", "")
     Fpassword = Fpassword.replace("]", "")
     temp =  Fpassword.split()
     for i in range(128):
-        temp[i] = float(temp[i])
+        print(temp[i])
+        temp[i] = np.float64(temp[i])
     return np.array(temp, dtype=float)
 
 #Hàm bảo mật
@@ -59,21 +80,24 @@ def Logingin():
         event, values = window.read()
         if event == "Login":
             soldier = 0
-            i = 1
-            usname = worksheet.cell(i, 1).value
-            psword = worksheet.cell(i, 2).value
+            row = 1
+            usname = worksheet.cell(row, USERNAME_COLUMN).value
+            psword = worksheet.cell(row, PASSWORD_COLUMN).value
             while usname != None:
-                if usname == Secure(str(values["username"])) and psword == Secure(str(values["password"])):
-                    soldier = 1   
-                    break
+                if usname == Secure(values["username"]): 
+                    if CheckPassword(psword, values["password"]):
+                        soldier = 1   
+                        break
                 else:
-                    i = i + 1
-                usname = worksheet.cell(i, 1).value
-                psword = worksheet.cell(i, 2).value
+                    row = row + 1
+                usname = worksheet.cell(row, USERNAME_COLUMN).value
+                psword = worksheet.cell(row, PASSWORD_COLUMN).value
             if soldier == 0:
                 window["confirm"].update("Fail")
             else:
                 window["confirm"].update("Success")
+                LoadData(c.money, c.language, c.selected_buff, c.winrate, values["username"])
+                print((c.money, c.language, c.selected_buff, c.winrate, values["username"]))
                 running = False
                 window.close()
                 return True
@@ -92,6 +116,10 @@ def Logingin():
 #Hàm đăng kí
 def Registerin():
     layout = [
+        [
+            sg.Text("Email"),
+            sg.Input(key= "email")
+        ],
         [
             sg.Text ("Username"),
             sg.Input (key= "username"),
@@ -119,22 +147,47 @@ def Registerin():
         if event == sg.WIN_CLOSED or event == "Exit":
             running = False
             window.close()
-
         if event == "Login":
             running = False
             window.close()
             break
         if event == "Create an account":
             if values["password"] == values["confirm"]:
-                values["username"] = Secure(str(values["username"]))
-                values["password"] = Secure(str(values["password"]))
-                values["confirm"] = Secure(str(values["confirm"]))
-                values_list = list(values.values())
-                worksheet.append_row(values_list)
-                sg.popup("Saved")
-                running = False
-                window.close()
-                break
+                soldier = 0
+                row = 1
+                usname = worksheet.cell(row, USERNAME_COLUMN).value
+                while usname != None:
+                    if usname == Secure(values["username"]):
+                        sg.popup("The account has already exist, please try another one or login in to continue")
+                        if FaceIDLogin():
+                            soldier = 1
+                            break
+                        else:
+                           soldier = 0
+                           row = row + 1
+                           break
+                    else:
+                        row = row + 1
+                        usname = worksheet.cell(row, USERNAME_COLUMN).value
+                        soldier = 1 
+                if soldier == 1 or row == 1:
+                    rannumber = random.randint(100000, 999999)
+                    SendEmail(rannumber, values["email"])
+                    if Verify(rannumber):
+                        values["email"] = Secure(values["email"])
+                        values["username"] = Secure(values["username"])
+                        values["password"] = Secure(values["password"])
+                        values["confirm"] = Secure(values["confirm"])
+                        worksheet.update_cell(row, EMAIL_COLUMN, str(values["email"]))
+                        worksheet.update_cell(row, USERNAME_COLUMN, str(values["username"]))
+                        worksheet.update_cell(row, PASSWORD_COLUMN, str(values["password"]))
+                        sg.popup("Saved")
+
+                        SaveGame(300, 'English', 0, 0, values["username"])
+
+                        running = False
+                        window.close()
+                        break
             else:
                 sg.popup("Please enter the same password in both password fields.")
 
@@ -181,20 +234,24 @@ def FaceIDLogin():
             inputedPic_encoding = face_recognition.face_encodings (inputedPic)[0]
         if event == "Login":
             soldier = 0
-            i = 1
-            usname = worksheet.cell(i, 1).value
+            row = 1
+            usname = worksheet.cell(row, USERNAME_COLUMN).value
             while usname != None:
-                if usname == Secure(str(values["username"])):
-                    psword = LoadFaceID(worksheet.cell(i, 4).value)
-                    if face_recognition.compare_faces([inputedPic_encoding], psword)[0]:
-                        soldier = 1   
-                        break
+                if usname == Secure(values["username"]):
+                    psword = LoadFaceID(worksheet.cell(row, FACE_ID_COLUMN).value)
+                    if not(SamePic(psword, LoadFaceID(str(inputedPic_encoding)))):
+                        if CheckFaceID(psword, inputedPic_encoding):
+                            soldier = 1   
+                            break
+                        else:
+                            break
                     else:
-                        i = i + 1
-                        usname = worksheet.cell(i,1).value
+                        window["confirm"].update("Please choose a photo different from the one used to register") 
+                        break
                 else:
-                    i = i + 1
-                    usname = worksheet.cell(i, 1).value
+                    row = row + 1
+                    usname = worksheet.cell(row, USERNAME_COLUMN).value
+            
             if soldier == 0:
                 window["confirm"].update("Fail")
             else:
@@ -228,28 +285,161 @@ def FaceIDRegisterin():
         event, values = window.read()
         if event == "Exit" or event == sg.WINDOW_CLOSED:
             running = False
-            window.close()            
+            window.close()
         if event == "Load picture":
             Tk().withdraw()
             inputedPic = face_recognition.load_image_file(askopenfilename())
             inputedPic_encoding = str(face_recognition.face_encodings (inputedPic)[0])
         if event == "Register":
             soldier = 0
-            i = 1
-            usname = worksheet.cell(i, 1).value
+            row = 1
+            usname = worksheet.cell(row, USERNAME_COLUMN).value
             while usname != None:
-                if usname == Secure(str(values["username"])):
-                    worksheet.update_cell(i, 4, inputedPic_encoding)
-                    soldier = 1
-                    sg.popup("Saved")
-                    running = False
-                    window.close()
-                    break
+                if usname == Secure(values["username"]):
+                    sg.popup("The account has already exist, please try another one or login in to continue")
+                    if Logingin():
+                        worksheet.update_cell(row, FACE_ID_COLUMN, inputedPic_encoding)
+                        soldier = 1
+                        sg.popup("Saved")
+                        running = False
+                        window.close()
+                        break
+                    else:
+                        soldier = 1
+                        break
                 else:
-                    i = i + 1
-                    usname = worksheet.cell(i, 1).value
+                    row = row + 1
+                    usname = worksheet.cell(row, USERNAME_COLUMN).value
             if soldier == 0:
-                worksheet.update_cell(i, 1, Secure(str(values["username"])))
-                worksheet.update_cell(i, 4, inputedPic_encoding)
+                worksheet.update_cell(row, USERNAME_COLUMN, Secure(values["username"]))
+                worksheet.update_cell(row, FACE_ID_COLUMN, inputedPic_encoding)
                 sg.popup("Saved")
-            
+                running = False
+                window.close()
+                break
+
+#Hàm lưu dữ liệu khi tắt game
+def SaveGame (MoneyV, LanguageV, BuffV, WinrateV, Username):
+    Row = 1
+    usname = worksheet.cell(Row, USERNAME_COLUMN).value
+    while usname != None:
+        if usname == Secure(Username):
+            Save(Row, MONEY_COLUMN, MoneyV)
+            Save(Row, LANGUAGE_COLUMN, LanguageV)
+            Save(Row, BUFF_COLUMN, BuffV)
+            Save(Row, WINRATE_COLUMN, WinrateV)
+        else:
+            Row = Row + 1
+
+#Hàm lấy dữ liệu khi khởi động game
+def LoadData (M, L, B, W, Usname):
+    Row = 1
+    usname = worksheet.cell(Row, USERNAME_COLUMN).value
+    while usname != None:
+        if usname == Secure(Usname):
+            M = worksheet.cell(Row, MONEY_COLUMN).value
+            L = worksheet.cell(Row, LANGUAGE_COLUMN).value
+            B = worksheet.cell(Row, BUFF_COLUMN).value
+            W = worksheet.cell(Row, WINRATE_COLUMN).value
+        else:
+            Row = Row + 1
+
+def CheckPassword (SavedPassword, InputedPassword):
+    if SavedPassword == Secure(InputedPassword):
+        return True
+    else:
+        return False
+    
+def CheckFaceID (SavedFaceID, InputedFaceID):
+    return face_recognition.compare_faces([SavedFaceID], InputedFaceID)[0]
+
+#Hàm gửi mail xác nhận
+def SendEmail(Code, Useremail):
+    sender_email = "racetracktycoon@gmail.com"
+    receiver_email = Useremail
+    password = "gzxy qgbw mulx vwux"
+
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "Confirm code"
+    message["From"] = "RACETRACK TYCOON"
+    message["To"] = receiver_email
+
+    # Create the plain-text and HTML version of your message
+    text = """\
+    Hello, 
+    This is the code you need to verify your email, 
+    Please enter this code to the "Verify code" section to continue:
+    www.realpython.com""" + str(Code)
+    html = """\
+    <html>
+    <body>
+        <p>Hello,<br>
+        This is the code you need to verify your email,<br>
+        Please enter this code to the "Verify code" section to continue: 
+        </p>
+    </body>
+    </html>
+    """ + str(Code) + """\
+    <html>
+    <body>
+        <p>To assure that your account is safe, please don't send this code to other people.
+        </p>
+    </body>
+    </html>
+    """
+
+    # Turn these into plain/html MIMEText objects
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+
+    # Add HTML/plain-text parts to MIMEMultipart message
+    # The email client will try to render the last part first
+    message.attach(part1)
+    message.attach(part2)
+
+    # Create secure connection with server and send email
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(
+            sender_email, receiver_email, message.as_string()
+        )
+
+#Hàm xác nhận
+def Verify(Vcode):
+    layout = [
+        [
+            sg.Text("Verification code:"),
+            sg.Input(key = "verification code"),
+        ],
+        [
+            sg.Button("Verify", expand_x = True),
+            sg.Button("Exit", expand_x= True)
+        ],
+        [
+            sg.Text("", key = "announcement"),
+        ]
+    ]
+    window = sg.Window("Verification", layout= layout)
+
+    running = True
+    while running:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED or event == "Exit":
+            running = False 
+            window.close()
+        if event == "Verify":
+            if values["verification code"] == str(Vcode):
+                window.close()
+                return True
+            else:
+                window["announcment"].update("Please enter the correct verification code")
+
+#Hàm check ảnh trùng
+def SamePic(Pic1, Pic2):
+    temp1 = list(Pic1)
+    temp2 = list(Pic2)
+    for i in range (128):
+        if temp1[i] != temp2[i]:
+            return False
+    return True
